08d8c168649907d101ff2f93eba7dbd4356528ad
Zac Smith <zrs3141592@gmail.com>
2020-09-25 16:42:45 -0600

    create LinkBox
    Decided to add a new component that will hold the links, and render on Short.jsx

---
+++ src/components/pages/LinkBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const LinkBox = () => {
+  return (
+    <div>
+      <div>
+        <p>old url</p>
+      </div>
+      <div>
+        <a href="#">short url</a>
+        <button>COPY</button>
+      </div>
+    </div>
+  )
+}
+
+export default LinkBox
